06f1828ac80bd70e05951daf4ffed633a99365f3
Raul250106 <raul.luna@uisek.edu.ec>
2026-01-31 19:55:05 -0500

    Agregar los modelos en la parte de admin para poder agregar desde admin y probar en postman

--- Movies/admin.py
+++ Movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Movie
+from .models import Director
 # Register your models here.
+@admin.register(Movie, Director)
+class MoviesAdmin(admin.ModelAdmin):
+    pass
